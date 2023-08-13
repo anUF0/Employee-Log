f00359fc140036cda325b775490234e4f44a37b7
Paolo van Reyk <paolo.vanreyk@gmail.com>
2023-08-14 02:39:14 +1000

    Inquirer code added

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE department(
 );
 
 CREATE TABLE role(
-id INT NOT NULL PRIMARY KEY,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INT,
@@ -17,9 +17,11 @@ FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
+    manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id)
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-const inquirer = require(inquirer);
+//Imports the necissaru libraries
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+//Connects to Database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password', //Change to your personal password
+    database: 'employees_db'
+},
+console.log('Connected to employees_db')
+);
+
+//A Series of Promts that Chooses how User will Interact with db
+inquirer.prompt([{
+    type: 'list',
+    name: 'prompt',
+    message: 'What would you like to do?',
+    choices: ['View All Department',
+            'View All Employees',
+            'View All Roles',
+            'Update An Employee Role',
+            'Add A Department',
+            'Add An Employee',
+            'Add New Deparment',
+            'Exit'
+        ]
+}]).then((answers)=>{
+const { choices } = answers;
+
+if (choices === 'View All Department') {
+    viewAllDepartments();
+}
+if (choices === 'View All Employees') {
+    viewAllEmployees();
+}
+if (choices === 'View All Roles') {
+    viewAllRoles();
+}
+if (choices === 'Update Employee Role') {
+    updateEmployeeRole();
+}
+if (choices === 'Add New Employee') {
+    addNewEmployee();
+}
+if (choices === 'Add New Role') {
+    addNewRole();
+}
+if (choices === 'Add New Department') {
+    addNewDepartment();
+}
+else{
+    console.log('Disconnected from employees_db')
+        db.end();
+}
+});
+
