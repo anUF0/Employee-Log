5d6ff88b3e4de2444974ce0a30640f886a9da514
Paolo van Reyk <paolo.vanreyk@gmail.com>
2023-08-14 11:19:02 +1000

    Add display functions and updated Schema

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Imports the necissaru libraries
-const mysql = require("mysql");
-const inquirer = require("inquirer");
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
 //Connects to Database
 const db = mysql.createConnection({
@@ -12,8 +12,11 @@ const db = mysql.createConnection({
 console.log('Connected to employees_db')
 );
 
+
+const returnPrompts = () => { 
 //A Series of Promts that Chooses how User will Interact with db
-inquirer.prompt([{
+inquirer.prompt([
+    {
     type: 'list',
     name: 'prompt',
     message: 'What would you like to do?',
@@ -24,35 +27,65 @@ inquirer.prompt([{
             'Add A Department',
             'Add An Employee',
             'Add New Deparment',
-            'Exit'
-        ]
-}]).then((answers)=>{
-const { choices } = answers;
-
-if (choices === 'View All Department') {
+            'Exit',
+        ],
+}
+]).then((answers)=>{
+const { choice } = answers;
+if (choice === 'View All Departments') {
     viewAllDepartments();
 }
-if (choices === 'View All Employees') {
+if (choice === 'View All Employees') {
     viewAllEmployees();
 }
-if (choices === 'View All Roles') {
+if (choice === 'View All Roles') {
     viewAllRoles();
 }
-if (choices === 'Update Employee Role') {
+if (choice === 'Update Employee Role') {
     updateEmployeeRole();
 }
-if (choices === 'Add New Employee') {
+if (choice === 'Add New Employee') {
     addNewEmployee();
 }
-if (choices === 'Add New Role') {
+if (choice === 'Add New Role') {
     addNewRole();
 }
-if (choices === 'Add New Department') {
+if (choice === 'Add New Department') {
     addNewDepartment();
 }
-else{
-    console.log('Disconnected from employees_db')
-        db.end();
+if(choice === 'Exit'){
+    console.log('Disconnected from employees_db');
+    db.end();
 }
-});
+})   
+};
 
+const viewAllDepartments = () => {
+    const query = 'SELECT * FROM departments';
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    })
+    returnPrompts();
+}
+
+const viewAllEmployees = () => {
+    const query = 'SELECT * FROM employees';
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    })
+    returnPrompts();
+}
+
+const viewAllRoles = () => {
+    const query = 'SELECT * FROM roles';
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    })
+    returnPrompts();
+}
+
+//Intisation
+returnPrompts();
