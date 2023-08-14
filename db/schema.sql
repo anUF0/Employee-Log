0f5a9b72d3368114ca9c3c6b34bd568fcf4b5597
Paolo van Reyk <paolo.vanreyk@gmail.com>
2023-08-14 12:54:51 +1000

    Updated Schema and Seeds

--- db/schema.sql
+++ db/schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE employees (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,
+    manager_id INT NULL,
     FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
---
+++ db/seed.sql
@@ -0,0 +1,34 @@
+-- Inserts Dummy Names into 'departments' Table --
+INSERT INTO departments(name) VALUES
+  ('Engineering'),
+  ('Finance'),
+  ('Legal'),
+  ('Sales');
+
+-- Inserts Dummy Names into 'roles' Table --
+INSERT INTO roles
+(title, salary, department_id)
+VALUES
+('Particle Beam Engineer', 125000, 1), -- 1 --
+('Propulsion Technician',100000, 1), -- 2 --
+('Accountant', 100000, 2), -- 3 --
+('Chef', 120000, 2), -- 4 --
+('Head of Legal', 120000, 2), -- 5 --
+('Prosecutor', 90000, 3), -- 6 --
+('Attorney', 110000, 3), -- 7 --
+('Salt Merchant', 50000, 4), -- 8 --
+('Sales Head', 125000, 4); -- 9 --
+
+--  Inserts Dummy Names into 'employees' Table --
+INSERT INTO employees
+(first_name, last_name, role_id, manager_id)
+VALUES
+('Jimmy','Schmitz',1, null), -- 1 --
+('Tem','Ray', 2, 1), -- 2 --
+('Evelin','Raine', 2, null),  -- 3 --
+('Derick','Savage', 4, 3),  -- 4 --
+('Cap','Com', 5, null),  -- 5 -- --
+('Phoenix','Wright', 7, 5),  -- 6 --
+('Miles','Edgeworth', 6, 5), -- 7 --
+('Barbra','White', 9, null),  -- 8 --
+('Joan', 'Fishhook', 8, 8);  -- 9 --
