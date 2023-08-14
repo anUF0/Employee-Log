2cc6c2cc88476fdd1e4c7128f6db622ab8f377e8
Paolo van Reyk <paolo.vanreyk@gmail.com>
2023-08-14 15:18:53 +1000

    Add Employees funciton added

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ inquirer.prompt([
             'View All Employees',
             'View All Roles',
             `Update an Employee's Role`,
-            'Add A Department',
-            'Add An Employee',
+            'Add New Department',
+            'Add New Employee',
             'Add New Deparment',
             'Exit',
         ],
@@ -130,6 +130,59 @@ inquirer.prompt([
 });
 };
 
+const addNewEmployee = () =>{
+    db.query('SELECT * FROM roles', (err, roles)=>{
+        if(err)console.log(err);
+        roles = roles.map((roles) => {
+        return {
+            name: roles.title,
+            value: roles.id,
+        };
+    });
+    db.query('SELECT * FROM employees', (err, employees)=>{
+        if(err)console.log(err);
+        employees = employees.map((employees) => {
+        return {
+            name: `${employees.first_name} ${employees.last_name}`,
+            value: employees.id,
+        };
+    });
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'Enter the First Name of the New Employee'
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'Enter the Last Name of the New Employee'
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is their Role?',
+            choices: roles,
+        },
+        {
+            type: 'list',
+            name: 'managerId',
+            message: 'Who their Manager?',
+            choices: employees,
+        }
+    ]).then((data) =>{
+        db.query('INSERT INTO employees SET ?',
+        {
+            first_name: data.firstName,
+            last_name: data.lastName,
+            role_id: data.role,
+            manager_id: data.managerId
+        })
+        viewAllEmployees();
+    })
+});
+});
+};
 
 //Intisation
 returnPrompts();
